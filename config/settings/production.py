728dac4decf3c8ba903786609906cb22991f4a09
Flavio Marcato <flavius476@gmail.com>
2017-09-10 14:37:02 +0200

    Set secret key for production use.

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,6 +3,9 @@ from .base import *
 import dj_database_url
 
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_env_variable('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -12,3 +15,4 @@ DEBUG = False
 DATABASES = {
     "default": dj_database_url.config(),
 }
+

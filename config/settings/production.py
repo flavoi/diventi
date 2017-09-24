b319693ea69350c1cc201780a8ddb1c471ec8d09
Flavio Marcato <flavius476@gmail.com>
2017-09-24 19:06:54 +0200

    Settings up Amazon S3.

--- config/settings/production.py
+++ config/settings/production.py
@@ -36,7 +36,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = ''
 
 STATIC_S3_PATH = 'static'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, STATIC_S3_PATH)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_ROOT = "/%s/" % STATIC_S3_PATH
 

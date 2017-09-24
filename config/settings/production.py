a4079f7a9a546f5ac84a61d70189cf1bcabf47be
Flavio Marcato <flavius476@gmail.com>
2017-09-24 15:18:20 +0200

    Setting up Amazon S3.

--- config/settings/production.py
+++ config/settings/production.py
@@ -32,6 +32,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+AWS_LOCATION = 'static'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
 DEFAULT_S3_PATH = 'media'

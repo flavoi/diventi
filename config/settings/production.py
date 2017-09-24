7fd06d4408b48f44e9e0774c89ae37deda311187
Flavio Marcato <flavius476@gmail.com>
2017-09-24 19:27:15 +0200

    Settings up Amazon S3.

--- config/settings/production.py
+++ config/settings/production.py
@@ -25,24 +25,25 @@ ALLOWED_HOSTS = [
 # Amazon S3 support
 # http://aws.amazon.com/
 
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+
 AWS_ACCESS_KEY_ID = get_env_variable('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = get_env_variable('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = get_env_variable('AWS_STORAGE_BUCKET_NAME')
+
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_QUERYSTRING_AUTH = False
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-AWS_LOCATION = ''
-
 STATIC_S3_PATH = 'static'
-STATIC_URL = '//%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, STATIC_S3_PATH)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_ROOT = "/%s/" % STATIC_S3_PATH
-
 DEFAULT_S3_PATH = 'media'
-MEDIA_URL = '//%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, DEFAULT_S3_PATH)
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
-MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
 
-ADMIN_MEDIA_PREFIX = 'admin/'
+STATIC_URL = '//%s/' % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_DIRS = (BASE_DIR / 'static' / 'diventi',)
+STATIC_ROOT = 'staticfiles'
+MEDIA_URL = STATIC_URL / '%s' % DEFAULT_S3_PATH
+MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'

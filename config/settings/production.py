e8533939bae4f8d28b52d280d2da7b2ea604c0dd
Flavio Marcato <flavius476@gmail.com>
2017-10-06 08:42:26 +0200

    Enabled debug mode.

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import *
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases

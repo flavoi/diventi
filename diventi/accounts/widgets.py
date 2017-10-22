6c8a3b27d6eb76b2c73d20268b127982536c49b6
Flavio Marcato <flavius476@gmail.com>
2017-10-22 22:10:41 +0200

    Added custom widget for account's avatar.

---
+++ diventi/accounts/widgets.py
@@ -0,0 +1,32 @@
+from django import forms
+
+from .models import DiventiAvatar
+
+class DiventiAvatarSelect(forms.Select):
+    
+    # option_template_name = 'django/forms/widgets/select_option.html'
+    # option_template_name = 'select_option.html'
+    
+    def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
+        index = str(index) if subindex is None else "%s_%s" % (index, subindex)
+        if attrs is None:
+            attrs = {}
+        option_attrs = self.build_attrs(self.attrs, attrs) if self.option_inherits_attrs else {}
+        option_attrs['data-img-alt'] = value
+        if value and int(value) > 0:
+            avatar = DiventiAvatar.objects.get(id=int(value))
+            option_attrs['data-img-src'] = avatar.image.url     
+        if selected:
+            option_attrs.update(self.checked_attribute)
+        if 'id' in option_attrs:
+            option_attrs['id'] = self.id_for_label(option_attrs['id'], index)                
+        return {
+            'name': name,
+            'value': value,
+            'label': label,
+            'selected': selected,
+            'index': index,
+            'attrs': option_attrs,
+            'type': self.input_type,
+            'template_name': self.option_template_name,
+        }

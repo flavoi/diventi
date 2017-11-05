285b353676f9d7a260c96e9a0ef4a8e6c3ff9661
Flavio Marcato <flavius476@gmail.com>
2017-11-05 13:04:14 +0100

    Added reset scripts for demo purposes.

---
+++ reset.sh
@@ -0,0 +1,4 @@
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+echo Migrations deleted.

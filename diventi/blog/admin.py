2954fbccb932cf6b5670336747b6dd2e7b33cd27
Flavio Marcato <flavius476@gmail.com>
2017-11-17 09:30:54 +0100

    Added admin action to publish articles.

--- diventi/blog/admin.py
+++ diventi/blog/admin.py
@@ -3,15 +3,17 @@ from django.contrib import admin
 from .models import Article, Category, Attachment
 
 
-class AttachmentInline(admin.TabularInline):
-    model = Attachment
-    fields = ('title', 'file')
+def make_published(modeladmin, request, queryset):
+    queryset.update(published=True)
+make_published.short_description = "Mark selected stories as published"
 
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'published', 'hot', 'publication_date']
     readonly_fields = ['created', 'modified', 'publication_date']
     prepopulated_fields = {"slug": ("title",)}   
+    actions = [make_published]
+
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Category)

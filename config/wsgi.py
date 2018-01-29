52e3f8d9e39e25e0548be9484a4cedde27b34386
flavoi <flavius476@gmail.com>
2018-01-29 23:31:52 +0100

    Updated wsgi for deployment.

--- config/wsgi.py
+++ config/wsgi.py
@@ -16,7 +16,7 @@ from django.core.wsgi import get_wsgi_application
 path = '/containers/30356/diventi'
 if path not in sys.path:
     sys.path.append(path)
-    
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diventi.settings")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production_sqlite")
 
 application = get_wsgi_application()

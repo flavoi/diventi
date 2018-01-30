c0c208b847174a9fb4f37d826530e69971a312ff
flavoi <flavius476@gmail.com>
2018-01-30 20:35:13 +0100

    Updated paths for deployment.

--- config/settings/production_sqlite.py
+++ config/settings/production_sqlite.py
@@ -6,6 +6,6 @@ from .production import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJ_ROOT, 'diventi.sqlite'),
+        'NAME': PROJ_ROOT / 'diventi.sqlite',
     }
 }
--- config/wsgi.py
+++ config/wsgi.py
@@ -13,10 +13,4 @@ import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-path = '/containers/30356/diventi'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production_sqlite")
-
 application = get_wsgi_application()

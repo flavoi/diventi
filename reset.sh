e447976e5483f1af20cd27a87d079257f4ab1136
Flavio Marcato <flavius476@gmail.com>
2018-02-19 08:19:46 +0100

    Removed unecessary files.

--- reset.sh
+++
@@ -1,4 +0,0 @@
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
-find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
-echo Migrations deleted.

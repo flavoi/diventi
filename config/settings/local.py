0a319fe1e92dfb36b2faba60dc26b647b2023291
Flavio Marcato <flavius476@gmail.com>
2018-06-17 10:31:38 +0200

    Enclosed smtp settings behind env variables.

--- config/settings/local.py
+++ config/settings/local.py
@@ -55,4 +55,15 @@ DEFAULT_FILE_STORAGE = 'diventi.core.storages.MediaStorage'
 MEDIA_URL = '//%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 # Email backend for testing and development
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST_KEY = get_env_variable('EMAIL_HOST_KEY')
+EMAIL_HOST_USER_KEY = get_env_variable('EMAIL_HOST_USER_KEY')
+EMAIL_HOST_PASSWORD_KEY = get_env_variable('EMAIL_HOST_PASSWORD_KEY')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = EMAIL_HOST_KEY # mail service smtp
+EMAIL_HOST_USER = EMAIL_HOST_USER_KEY # email id
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD_KEY # password
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True

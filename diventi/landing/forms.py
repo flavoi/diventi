9a358dd022897346935d6c734713a53942569eb9
flavoi <flavius476@gmail.com>
2019-03-25 04:26:30 -0700

    Updated featured link choices algotithm.

--- diventi/landing/forms.py
+++ diventi/landing/forms.py
@@ -9,13 +9,15 @@ class PresentationForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(PresentationForm, self).__init__(*args, **kwargs)
-        featured_product = Product.objects.featured()
-        featured_survey = Survey.objects.featured()
         LINK_CHOICES = {
             ('', '----'),
-            (featured_product.get_lazy_absolute_url(), '%s/%s' % (featured_product.class_name(), featured_product)),
-            (featured_survey.get_lazy_absolute_url(), '%s/%s' % (featured_survey.class_name(), featured_survey)),
         }
+        featured_product = Product.objects.featured()
+        if featured_product:
+            LINK_CHOICES.update([(featured_product.get_lazy_absolute_url(), '%s/%s' % (featured_product.class_name(), featured_product))])
+        featured_survey = Survey.objects.featured()
+        if featured_survey:
+            LINK_CHOICES.update([(featured_survey.get_lazy_absolute_url(), '%s/%s' % (featured_survey.class_name(), featured_survey))])
         self.fields['featured_link'].widget = forms.Select(
             choices = LINK_CHOICES
             )

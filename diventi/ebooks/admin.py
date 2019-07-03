295337dfc8d2531058a80916d80557013fd5f08c
flavoi <flavius476@gmail.com>
2019-07-02 23:50:03 -0700

    Enabled book description in ebook page.

--- diventi/ebooks/admin.py
+++ diventi/ebooks/admin.py
@@ -111,7 +111,7 @@ class BookAdmin(DiventiTranslationAdmin):
             'fields': ('col_lg', 'col_md',)
         }),
         (_('Editing'), {
-            'fields': ('title', 'short_title', 'slug', 'created', 'modified', 'publication_date'),
+            'fields': ('title', 'short_title', 'description', 'slug', 'created', 'modified', 'publication_date'),
         }),
     )
     readonly_fields = ['created', 'modified', 'publication_date']

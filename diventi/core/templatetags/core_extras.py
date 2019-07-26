57e39f933c4e076388c0bfd0de2ddca493f57c85
flavoi <flavius476@gmail.com>
2019-07-25 23:38:18 -0700

    Updated review section.

---
+++ diventi/core/templatetags/core_extras.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.filter()
+def to_int(value):
+    return int(value)
+
+@register.filter()
+def to_float(value):
+    return float(value)

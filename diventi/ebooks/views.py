d1370f6fbc9ce327677133ef6171aa8d2e0fee70
flavoi <flavius476@gmail.com>
2019-08-05 00:16:47 -0700

    Updated navigation order.

--- diventi/ebooks/views.py
+++ diventi/ebooks/views.py
@@ -88,11 +88,11 @@ class ChapterDetailView(LoginRequiredMixin, UserHasProductMixin,
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         chapters = context['chapters']
-        next_chapter = chapters.filter(id__gt=self.object.id)
+        next_chapter = chapters.filter(order_index__gt=self.object.id)
         next_chapter = next_chapter.order_by('order_index')
         next_chapter = next_chapter.first()
         context['next_chapter'] = next_chapter
-        previous_chapter = chapters.filter(id__lt=self.object.id)
+        previous_chapter = chapters.filter(order_index__lt=self.object.id)
         previous_chapter = previous_chapter.order_by('-order_index')
         previous_chapter = previous_chapter.first()
         context['previous_chapter'] = previous_chapter

b00f051d78a4529e85561d6f46a69a5f15a215bd
flavoi <flavius476@gmail.com>
2020-03-08 10:06:07 -0700

    Brave: improved support for locale files.

--- diventi/brave/views.py
+++ diventi/brave/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 
 def file_challenge(request):
-    f = open(settings.PROJ_ROOT / 'brave-rewards-verification.txt', 'r')
+    f = open(settings.BASE_DIR / 'brave/challenges' / request.LANGUAGE_CODE / 'brave-rewards-verification.txt', 'r')
     file_content = f.read()
     f.close()
     return HttpResponse(file_content, content_type="text/plain")

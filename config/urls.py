0f9f7c7e43180c8000cf490c832fd0a41657b771
flavoi <flavius476@gmail.com>
2021-04-07 00:06:56 -0700

    Updated illustrations on confirmation pages.

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import gettext_lazy as _
 from django.views.i18n import JavaScriptCatalog
+from django.views.generic import TemplateView
 
 from machina import urls as machina_urls
 
@@ -27,6 +28,9 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [
     path('payments/', include('diventi.payments.urls', namespace='payments')),
     path('apps/', include('diventi.products.urls', namespace='products_retro')), # Duplicated to preserve past shared links
+    path('403/', TemplateView.as_view(template_name='403.html')),
+    path('404/', TemplateView.as_view(template_name='404.html')),
+    path('500/', TemplateView.as_view(template_name='500.html')),
 ]
 
 urlpatterns += i18n_patterns(

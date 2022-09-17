8c5e953526f32e9bed06e1b761f9e3c1776d27b4
flavoi <flavius476@gmail.com>
2022-09-17 14:16:26 +0000

    Updated url conf for public surveys.

--- diventi/feedbacks/urls.py
+++ diventi/feedbacks/urls.py
@@ -15,12 +15,14 @@ app_name = 'feedbacks'
 
 urlpatterns = [
     path(_('<slug:slug>/new-submit/'), new_answers_gate, name='new_answers_gate'),
-    path(_('<slug:slug>/questions/'), survey_questions, name='questions'),
-    path(_('<slug:slug>/questions/<str:author_name>/'), survey_questions, name='questions_author'),
+    path(_('<slug:slug>/questions/gate/'), survey_questions, name='questions'),
+    path(_('<slug:slug>/questions/gate/<str:author_name>/'), survey_questions, name='questions_author'),
     path(_('<slug:slug>/questions/private/'), survey_questions_private, name='questions_private'),
-    path(_('<slug:slug>/questions/<str:author_name>/private/'), survey_questions_private, name='questions_author_private'),
+    path(_('<slug:slug>/questions/private/<str:author_name>/'), survey_questions_private, name='questions_author_private'),
     path(_('<slug:slug>/questions/public/'), survey_questions_public, name='questions_public'),
-    path(_('<slug:slug>/questions/<str:author_name>/public/'), survey_questions_public, name='questions_author_public'),
+    path(_('<slug:slug>/questions/public/<str:author_name>/'), survey_questions_public, name='questions_author_public'),
     path(_('<slug:slug>/answers/'), AnswerListView.as_view(), name='answers'),
     path(_('<slug:slug>/survey-done/'), survey_done_quick, name='survey_done')
+    
+
 ]

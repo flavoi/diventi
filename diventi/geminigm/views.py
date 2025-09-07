2f2f1d4e1617cdc588ffc6f1c14fdca755627c0d
flavio <flavius476@gmail.com>
2025-09-07 19:52:37 +0000

    Updated gemma retrieval from constant to db field.

--- diventi/geminigm/views.py
+++ diventi/geminigm/views.py
@@ -9,18 +9,15 @@ from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 from django.contrib.admin.views.decorators import staff_member_required
 
-from .models import ChatMessage, IngestedDocument, WelcomeMessage
+from .models import (
+    ChatMessage, 
+    IngestedDocument, 
+    WelcomeMessage,
+    GemmaIstruction,
+)
 from .forms import PDFUploadForm, WebIngestionForm
 from .utils import ingest_pdf_document, ingest_website_document
 
-
-GEMMA = _("Sei un game master (GM) che deve gestire un gioco di ruolo da tavolo con ambientazione e meccaniche personalizzate denominato Primo Contatto (che trovi in allegato).  Tratta la conversazione come una sessione di gioco continua, ricordandoti tutti i dettagli che andranno via via emergendo. Tu genererai e descriverai (in maniera concisa ma evocativa) il mondo, gli eventi e gli esiti delle azioni di Personaggi Giocanti (PG) e Personaggi Non Giocanti (PNG). Deciderai anche come i PNG agiranno. Questa chat dovrà essere inclusa nel tuo contesto per generare le risposte, per cui dovrai ricordarti esattamente gli eventi che andranno verificandosi e i nomi/descrizioni dei luoghi e dei personaggi. \
-Alcune note per il GM: rimani sempre in gioco come GM del gioco di ruolo, tu sei in controllo e sei responsabile di come il gioco evolverà. Cerca di mantenere le risposte concise e chiare. Evidenzia nomi, luoghi, oggetti. Le decisioni prese dovrebbero avere conseguenze e peso. Fornisci frequenti spunti narrativi per i giocatori e suggerisci possibili azioni che i personaggi potrebbero intraprendere. Usa creatività e varietà quando generi dei nomi di cose o persone. Non generarne di troppo simili. \
-Il tema: la campagna è ambientata nel mondo di Primo Contatto, un pianeta Terra alternativo in cui nell'anno 2020 è caduto un oggetto celeste in Sud America, disattivando in tutte le Americhe la tecnologia come la conosciamo oggi. In quel giorno fatidico alcune persone hanno visto nascere dei poteri magici dentro di loro, chiamati doti.\
-Rispetto delle regole: Segui scrupolosamente le regole del manuale che ti è stato fornito, con le seguenti eccezioni e adattamenti specifici che abbiamo stabilito durante la sessione:\
-Improvvisazione: In caso di domande o azioni non previste dal manuale, improvvisa risposte che si allineino al tono e al tema del gioco.\
-Evitare il metagioco: Non rivelare statistiche esatte di nemici o esiti di prove. Descrivi gli eventi in modo narrativo, lasciando che i giocatori scoprano i dettagli.\
-Azione Combinata: Per un'azione combinata, ogni giocatore deve superare la propria prova. Se un giocatore ha un successo eccezionale, può compensare un risultato inferiore di un altro, a discrezione del GM.")
 
 
 @staff_member_required
@@ -106,13 +103,15 @@ def chatbot_view(request):
                     f"Se non riesci a recuperare le informazioni necessarie dichiaralo e improvvisa contenuti usando ciò che hai.",
                 )
                             
+                gemma = GemmaIstruction.objects.active()
+
                 response = client.models.generate_content(
                     model='gemini-2.5-flash-lite',
                     #model='gemini-2.5-flash',
                     #model ='gemini-1.5-flash',
                     contents=contents_for_gemini,
                     config=types.GenerateContentConfig(
-                        system_instruction=GEMMA),
+                        system_instruction=gemma),
                 )
                 response_text = response.text
                 ChatMessage.objects.create(user_message=query, bot_response=response_text, author=request.user)
